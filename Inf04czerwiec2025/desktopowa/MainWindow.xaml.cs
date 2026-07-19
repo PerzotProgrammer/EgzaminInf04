aece1c675d2bd276e7f58412afb3856fd4c28196
PerzotProgrammer <bartoszperz2007@gmail.com>
2026-07-19 22:24:26 +0200

    Przesłano egzamin inf04 czerwiec 2025

---
+++ Inf04czerwiec2025/desktopowa/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace desktopowa
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (!IsInitialized) return;
+
+            RectColor.Fill = new SolidColorBrush(GetCurrentColor());
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (!IsInitialized) return;
+
+            Color color = GetCurrentColor();
+
+            PreviewLabel.Background = new SolidColorBrush(color);
+
+            PreviewLabel.Content = $"{color.R},{color.G},{color.B}";
+        }
+
+        private Color GetCurrentColor()
+        {
+            byte red = (byte)SliderR.Value;
+            byte green = (byte)SliderG.Value;
+            byte blue = (byte)SliderB.Value;
+
+            SliderRLabel.Content = red;
+            SliderGLabel.Content = green;
+            SliderBLabel.Content = blue;
+
+            return Color.FromArgb(255, red, green, blue);
+        }
+    }
+}
---
+++ Inf04czerwiec2025/konsolowa/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace konsolowa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool firstPass = true;
+            int n;
+            do
+            {
+                if (!firstPass) Console.WriteLine("NIEPRAWIDŁOWA LICZBA");
+                else firstPass = false;
+
+                Console.Write("Ile wygenerować losowań?: ");
+
+            } while (!int.TryParse(Console.ReadLine(), out n) && n < 0);
+
+            Console.WriteLine("Zestawy wylosowanych liczb");
+
+            DescribeArray(GenerateArray(n));
+        }
+
+
+        /*
+         **********************************************
+         nazwa funkcji: GenerateArray
+         opis funkcji: Generuje tablice dwuwymiarową o n rzędach i 6 kolumnach
+         parametry: int n - liczba rzędów (liczba całkowita, powinna być większa od 0)
+         zwracany typ i opis: int[][] - tablica dwuwymiarowa o n rzędach i 6 kolumnach 
+            z liczbami losowymi z zakresu 1, 49
+         autor: 00000000000
+         ***********************************************
+         */
+        static int[][] GenerateArray(int n)
+        {
+            Random random = new Random();
+            int[][] arr = new int[n][];
+
+            for (int i = 0; i < n; i++) arr[i] = new int[6];
+
+            foreach (int[] row in arr)
+                for (int j = 0; j < row.Length; j++) row[j] = random.Next(1, 50);
+
+            return arr;
+        }
+
+        static void DescribeArray(int[][] arr)
+        {
+            int[] occurences = new int[50];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"Losowanie {i + 1}: ");
+                for (int j = 0; j < arr[i].Length; j++)
+                {
+                    Console.Write($"{arr[i][j]} ");
+                    occurences[arr[i][j]]++;
+                }
+                Console.WriteLine();
+            }
+
+            for (int i = 1; i < 50; i++)
+                Console.WriteLine($"Wystąpienia liczby {i}: {occurences[i]}");
+        }
+    }
+}
